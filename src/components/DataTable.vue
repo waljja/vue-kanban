d18e7fb136b6189888f86d75ff29e509d62a486b
waljja <2576723618@qq.com>
2023-01-12 13:12:25 +0800

    表格筛选

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,7 +1,7 @@
 <template>
   <el-table class="table" 
     size="small"
-    :data="tableData" 
+    :data="data.tableData" 
     :header-row-style="headerStyle"
     :header-cell-style="headerCellStyle"
     :row-class-name="tableRowClassName">
@@ -22,6 +22,7 @@
 </template>
 
 <script lang="ts" setup>
+import { reactive, computed } from "vue";
 import type { TableColumnCtx } from "element-plus";
 
 interface User {
@@ -58,161 +59,184 @@ const headerCellStyle = ({
   padding: 0
 })
 
-const tableData: User[] = [
-  {
-    item: 1,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000001',
-    uid: '000001596045',
-    receivingnumber: 100,
-    receivingTime: '2016-05-03',
-    state: '1'
-  },
-  {
-    item: 2,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000002',
-    uid: '000001611303',
-    receivingnumber: 200,
-    receivingTime: '2016-05-02',
-    state: '3'
-  },
-  {
-    item: 3,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000003',
-    uid: '000001605630',
-    receivingnumber: 300,
-    receivingTime: '2016-05-04',
-    state: '2'
-  },
-  {
-    item: 4,
-    partnumber: '610-LT10002-00R1',
-    batchId: '000004',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 5,
-    partnumber: '610-LT10002-00R1',
-    batchId: '000005',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 6,
-    partnumber: '610-LT10002-00R1',
-    batchId: '000394',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 7,
-    partnumber: '610-TAGA001-00R1',
-    batchId: '00009',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 8,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000393',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 9,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000028',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 10,
-    partnumber: '610-TAGA001-00R1',
-    batchId: '000175',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 11,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000174',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 12,
-    partnumber: '	620-LUHE001-00R3',
-    batchId: '15',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 13,
-    partnumber: '	620-LUHE001-00R3',
-    batchId: '00001',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 14,
-    partnumber: '620-PURP004-00R1',
-    batchId: '00007',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 15,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000030',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 16,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000031',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-  {
-    item: 17,
-    partnumber: '620-PURP004-00R1',
-    batchId: '000169',
-    uid: '000001605630',
-    receivingnumber: 400,
-    receivingTime: '2016-05-01',
-    state: '0'
-  },
-]
+const data = reactive({
+  tableData: [
+    {
+      item: 1,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000001',
+      uid: '000001596045',
+      receivingnumber: 100,
+      receivingTime: '2016-05-03',
+      state: '1'
+    },
+    {
+      item: 2,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000002',
+      uid: '000001611303',
+      receivingnumber: 200,
+      receivingTime: '2016-05-02',
+      state: '3'
+    },
+    {
+      item: 3,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000003',
+      uid: '000001605630',
+      receivingnumber: 300,
+      receivingTime: '2016-05-04',
+      state: '2'
+    },
+    {
+      item: 4,
+      partnumber: '610-LT10002-00R1',
+      batchId: '000004',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 5,
+      partnumber: '610-LT10002-00R1',
+      batchId: '000005',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 6,
+      partnumber: '610-LT10002-00R1',
+      batchId: '000394',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 7,
+      partnumber: '610-TAGA001-00R1',
+      batchId: '00009',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 8,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000393',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 9,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000028',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 10,
+      partnumber: '610-TAGA001-00R1',
+      batchId: '000175',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 11,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000174',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 12,
+      partnumber: '620-LUHE001-00R3',
+      batchId: '15',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 13,
+      partnumber: '620-LUHE001-00R3',
+      batchId: '00001',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 14,
+      partnumber: '620-PURP004-00R1',
+      batchId: '00007',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 15,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000030',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 16,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000031',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+    {
+      item: 17,
+      partnumber: '620-PURP004-00R1',
+      batchId: '000169',
+      uid: '000001605630',
+      receivingnumber: 400,
+      receivingTime: '2016-05-01',
+      state: '0'
+    },
+  ]
+})
+
+// partNumber 去重
+const pnFilters = computed(() => {
+  let pnArr: string[] = []
+  let pnDistinctArr: string[] = []
+  let pnNewArr: object[] = []
+  // 所有 partNumber 抽出来装入集合
+  data.tableData.forEach(item => {
+    pnArr.push(item.partnumber)
+  })
+  // 去重
+  pnDistinctArr = Array.from(new Set(pnArr))
+  // 装入 filters 数组
+  pnDistinctArr.forEach(item => {
+    pnNewArr.push({
+      text: item,
+      value: item
+    })
+  })
+  return pnNewArr
+})
 
 const filterPn = (
   value: any,
