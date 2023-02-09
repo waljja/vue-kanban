0171f0fee2115ba52b56d9e958a5e2f5796d8e81
waljja <2576723618@qq.com>
2023-02-10 01:03:11 +0800

    volar代码规范(datatable)

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,26 +1,44 @@
 <template>
-  <el-table class="table" 
+  <el-table
+    class="table"
     size="small"
     :header-row-style="headerStyle"
     :header-cell-style="headerCellStyle"
-    :row-class-name="tableRowClassName">
-    <el-table-column prop="item" label="Item" width="180" align="center" sortable/>
-      <!-- :filters="pnFilters" -->
-    <el-table-column 
-      prop="partNumber" 
-      label="PartNumber" 
-      width="180" 
-      align="center" 
-      :filter-method="filterHandler" />
+    :row-class-name="tableRowClassName"
+  >
+    <el-table-column prop="item" label="Item" width="180" align="center" sortable />
+    <!-- :filters="pnFilters" -->
+    <el-table-column
+      prop="partNumber"
+      label="PartNumber"
+      width="180"
+      align="center"
+      :filter-method="filterHandler"
+    />
     <!-- :filters="woFilters"  -->
-    <el-table-column prop="wo" label="工单" align="center" :filter-method="filterHandler" />
+    <el-table-column
+      prop="wo"
+      label="工单"
+      align="center"
+      :filter-method="filterHandler"
+    />
     <el-table-column prop="uid" label="UID" align="center" />
     <el-table-column prop="batch" label="批次号" align="center" />
     <el-table-column prop="quantity" label="数量" align="center" />
     <el-table-column prop="plant" label="工厂" align="center" />
     <!-- :filters="stateFilters"  -->
-    <el-table-column prop="state" label="状态" align="center" :filter-method="filterHandler" />
-    <el-table-column prop="storageLoc" label="仓位" align="center" :filter-method="filterHandler" />
+    <el-table-column
+      prop="state"
+      label="状态"
+      align="center"
+      :filter-method="filterHandler"
+    />
+    <el-table-column
+      prop="storageLoc"
+      label="仓位"
+      align="center"
+      :filter-method="filterHandler"
+    />
     <el-table-column prop="recTime" label="收货时间" align="center" sortable />
   </el-table>
 </template>
@@ -29,17 +47,17 @@
 import type { TableColumnCtx } from "element-plus";
 
 interface Product {
-  [key: string]: any
-  item: number
-  partNumber: string
-  wo: string
-  uid: string
-  batch: string
-  quantity: number
-  plant: string
-  state: number
-  storageLoc: string
-  recTime: string
+  [key: string]: any;
+  item: number;
+  partNumber: string;
+  wo: string;
+  uid: string;
+  batch: string;
+  quantity: number;
+  plant: string;
+  state: number;
+  storageLoc: string;
+  recTime: string;
 }
 
 // 父组件传递数组
@@ -49,29 +67,24 @@ interface Product {
 
 // const tableData = computed(() => props.data)
 
-const tableRowClassName = ({
-  row
-}: {
-  row: Product
-  rowIndex: number
-}) => {
+const tableRowClassName = ({ row }: { row: Product; rowIndex: number }) => {
   if (row.state === 2) {
-    return 'warning-row'
+    return "warning-row";
   } else if (row.state === 0) {
-    return 'send-row'
+    return "send-row";
   }
-  return 'success-row'
-}
+  return "success-row";
+};
 
-const headerStyle = ({
-  height: '50px',
-  color: '#000000',
-  'font-size': '20px'
-})
+const headerStyle = {
+  height: "50px",
+  color: "#000000",
+  "font-size": "20px",
+};
 
-const headerCellStyle = ({
-  padding: 0
-})
+const headerCellStyle = {
+  padding: 0,
+};
 
 // // filters 去重
 // const distinct = (array: string[]) => {
@@ -122,23 +135,19 @@ const headerCellStyle = ({
 //   return distinct(stateArr)
 // })
 
-const filterHandler = (
-  value: any,
-  row: Product,
-  column: TableColumnCtx<Product>
-) => {
-  const property = column['property']
-  return row[property] === value
-}
+const filterHandler = (value: any, row: Product, column: TableColumnCtx<Product>) => {
+  const property = column["property"];
+  return row[property] === value;
+};
 </script>
 
 <style>
 .el-table {
-  --el-table-row-hover-bg-color: #3CB371;
+  --el-table-row-hover-bg-color: #3cb371;
 }
 
 .el-table .warning-row {
-  --el-table-tr-bg-color: #DC143C;
+  --el-table-tr-bg-color: #dc143c;
 }
 
 .el-table .success-row {
@@ -146,11 +155,11 @@ const filterHandler = (
 }
 
 .el-table .send-row {
-  --el-table-tr-bg-color: #3CB371;
+  --el-table-tr-bg-color: #3cb371;
 }
 
 .table {
   font-size: 15px;
-  color: #FFFFFF;
+  color: #ffffff;
 }
 </style>
